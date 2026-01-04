1571aa931b83dd826612a7360bcedec21df19a59
techbhar-solutions <surajkumarmehto01@gmail.com>
2026-01-04 21:05:07 +0530

    Set up ViewFlipper with animation
    Initialize ViewFlipper with slide-in animation.

---
+++ ViewFlipper.java
@@ -0,0 +1,34 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.ViewFlipper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ScrollView scroll_view1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+      ViewFlipper viewFlipper = findViewById(R.id.viewFlipper);
+
+        viewFlipper.setInAnimation(this, android.R.anim.slide_in_left);
+
+    }
+
+}
