2b6cea5dfa263b4d9d5b3935d1481c1b95560477
techbhar-solutions <surajkumarmehto01@gmail.com>
2026-01-04 19:55:13 +0530

    Add ScrollView implementation in MainActivity
    Implemented a ScrollView in MainActivity with proper window insets handling.

---
+++ ScrollView.java
@@ -0,0 +1,33 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.widget.ScrollView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ScrollView scroll_view1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        scroll_view1 = findViewById(R.id.scroll_view);
+        scroll_view1.setFillViewport(true);
+        scroll_view1.smoothScrollTo(0, 800);
+
+    }
+
+}
