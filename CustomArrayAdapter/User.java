0163ef06f1c930ca004fee9d0b0173336514e343
techbhar-solutions <surajkumarmehto01@gmail.com>
2026-01-08 18:34:19 +0530

    Add User class for user data representation

---
+++ CustomArrayAdapter/User.java
@@ -0,0 +1,12 @@
+package com.example.myapp;
+
+public class User {
+    String name;
+    String email;
+    int image;
+    public User(String name, String email, int image) {
+        this.name = name;
+        this.email = email;
+        this.image = image;
+    }
+}
