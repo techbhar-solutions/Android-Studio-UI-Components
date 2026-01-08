c15e27f80685b64a36e41a37612cbc9cf4222600
techbhar-solutions <surajkumarmehto01@gmail.com>
2026-01-08 20:47:34 +0530

    Add User class with name and image fields

---
+++ BaseAdapter/User.java
@@ -0,0 +1,10 @@
+package com.example.myapp;
+
+public class User {
+    String name;
+    int image;
+    public User(String name, int image) {
+        this.name = name;
+        this.image = image;
+    }
+}
