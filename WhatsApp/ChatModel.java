33abcdcb1ef10c808ef47abd3c6665a590de924a
techbhar-solutions <surajkumarmehto01@gmail.com>
2026-01-09 17:50:22 +0530

    Create ChatModel.java

---
+++ WhatsApp/ChatModel.java
@@ -0,0 +1,10 @@
+package com.example.myapp;
+
+public class ChatModel {
+    String sender,message,time;
+    public ChatModel(String sender, String message, String time) {
+        this.sender = sender;
+        this.message = message;
+        this.time = time;
+    }
+}
