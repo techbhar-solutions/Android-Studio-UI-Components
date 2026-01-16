ffaa8f8daaa771fe87ff4dce5c00c2d17bda4bce
techbhar-solutions <surajkumarmehto01@gmail.com>
2026-01-16 21:11:13 +0530

    Implement MainActivity with fragment navigation

---
+++ Fragments/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.retrofitexample;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+     loadFragment(new HomeFragment());
+        bottomNav.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.homeactivity) {
+                loadFragment(new HomeFragment());
+                return true;
+            }
+            if (id == R.id.profileactivity) {
+                loadFragment(new ProfileFragment());
+                return true;
+            }
+            return false;
+    });
+    }
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+
+
+}
