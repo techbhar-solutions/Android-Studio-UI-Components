256f3c7da5a5b671a3df5f5f55084d1b6ab2f63c
techbhar-solutions <surajkumarmehto01@gmail.com>
2026-01-16 12:07:15 +0530

    Add PostModel class for Retrofit example

---
+++ Retrofit/PostModel.java
@@ -0,0 +1,18 @@
+package com.example.retrofitexample;
+
+public class PostModel {
+    private int   userId;
+    private int id;
+    private String title;
+    private String body;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody(){
+        return body;
+    }
+
+
+}
