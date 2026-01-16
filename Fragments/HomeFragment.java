0619bdf76fd778a8ae55d99ab1ac462f724a446a
techbhar-solutions <surajkumarmehto01@gmail.com>
2026-01-16 21:08:54 +0530

    Add HomeFragment class for home screen UI

---
+++ Fragments/HomeFragment.java
@@ -0,0 +1,19 @@
+package com.example.retrofitexample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+}
