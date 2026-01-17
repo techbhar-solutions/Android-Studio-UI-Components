a1c6ca418195cbb14cff2b04cf3140bfcbb185dc
techbhar-solutions <surajkumarmehto01@gmail.com>
2026-01-17 11:30:57 +0530

    Implement ViewPager with fragments in MainActivity

---
+++ ViewPager_With_Fragments/ActivityMain.java
@@ -0,0 +1,55 @@
+package com.example.retrofitexample;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.SystemBarStyle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this, SystemBarStyle.dark(Color.parseColor("#2D3C59")),
+                SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT)
+                );
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+
+
+    }
+
+
+
+}
