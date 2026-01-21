18a327757fce8f61a3cb292be23312b075be5b61
techbhar-solutions <surajkumarmehto01@gmail.com>
2026-01-21 11:51:22 +0530

    Location Permission Allow Karvana using code

---
+++ Location-Permision/HomeFragment.java
@@ -0,0 +1,53 @@
+package com.example.retrofitexample;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import org.jspecify.annotations.Nullable;
+
+public class HomeFragment extends Fragment {
+
+    private final ActivityResultLauncher<String[]> locationPermissionRequest =
+            registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), permissions -> {
+                Boolean fineLocationGranted = permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
+                Boolean coarseLocationGranted = permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false);
+                if (fineLocationGranted != null && fineLocationGranted) {
+                    // Precise location access granted
+                    Toast.makeText(getContext(), "Precise Location Granted", Toast.LENGTH_SHORT).show();
+                } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                    // Only approximate location access granted
+                    Toast.makeText(getContext(), "Approximate Location Granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    // No location access granted
+                    Toast.makeText(getContext(), "Location Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.homeview, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button mybtn = view.findViewById(R.id.button);
+
+        mybtn.setOnClickListener(v -> {
+            locationPermissionRequest.launch(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        });
+    }
+}
